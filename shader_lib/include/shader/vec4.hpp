435626ee0e385e697c7505ab3116e9c0444afc43
Mateusz Stadnik <matgla@live.com>
2021-10-05 22:38:03 +0200

    shader library

---
+++ shader_lib/include/shader/vec4.hpp
@@ -0,0 +1,42 @@
+/*
+ *   Copyright (c) 2021 Mateusz Stadnik
+
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "vec3.hpp"
+
+extern "C"
+{
+
+    struct vec4
+    {
+        vec4() = default;
+        vec4(const vec3 &v, float w_)
+            : x(v.x)
+            , y(v.y)
+            , z(v.z)
+            , w(w_)
+        {
+        }
+
+        float x;
+        float y;
+        float z;
+        float w;
+    };
+
+} // extern "C"
